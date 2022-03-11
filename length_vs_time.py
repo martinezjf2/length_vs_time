d4bcfb8953d99b9f318e97cb085b120c085ac45e
Jeffrey Martinez <martinezjf2@gmail.com>
2022-03-10 21:23:22 -0500

    divided up into get functions and calculation functions

--- length_vs_time.py
+++ length_vs_time.py
@@ -1,18 +1,10 @@
 # Program to calculate Length of the wire
-
 # Jeffrey Martinez
-
 # 3/08/2022
 
 
 
-weight = 454 * float(input("what is the weight in pounds: "))
 
-#weight = input("what is the weight in pounds: ")
-
-#weight_pounds = float(weight)
-
-#weight_grams = 454*weight_pounds
 
 density_list = ("Materials Density List ", "Aluminium: 2.74gm/cc ", "Brass8020: 8.49gm/cc", "Copper: 8.96gm/cc", "Gut: 1.27gm/cc", "KanthanID: 7.25gm/cc",
 
@@ -20,74 +12,100 @@ density_list = ("Materials Density List ", "Aluminium: 2.74gm/cc ", "Brass8020: 
 
       "SS430: 7.75gm/cc", "SS304: 8.03gm/cc", "Steel: 7.81gm/cc", "Titanium: 4.40gm/cc", "Tungsten: 19.20gm/cc", "Zyex: 1.30gm/cc" , "33%Al67%Cu: 6.89")
 
-for lang in density_list:
-      print(lang)
+
+
+
+# Get Data for calculations
+
+def greeting():
+      print("Welcome to the D'Addario String Calculator")
       
-density = float(input("what is the density in gram/cm3: "))
-wire_diameter = float(input("what is the diameter of the wire in inches: "))
-RPM = float(input("what is the RPM: "))
+def calculateWeight():
+      weight = 454 * float(input("what is the weight in pounds: "))
+      return weight
+
+def printDensityList():
+      for lang in density_list:
+            print(lang)
+
+def getDensity():
+      density = float(input("what is the density in gram/cm3: "))
+      return density
+
+def getWireDiameter():
+      wire_diameter = float(input("what is the diameter of the wire in inches: "))
+      return wire_diameter
+
+def getCoreDiameter():
+      core_diameter = float(input("core diameter in inches: "))
+      return core_diameter
+
+def getRpm():
+      rpm = float(input("what is the RPM: "))
+      return rpm
+
+def getStringLength():
+      string_length = float(input("What is the wrap length of the string in inches: "))
+      return string_length
+      
+# Calculations
+
+def calculateWireRadius():
+      wire_radius = (1/2) * wire_diameter
+      print("Radius of the wire: ", wire_radius)
+
+def calculateWireVolume():
+      wire_volume = weight/(density*2.54**3)
+      print("Volume of the wire: ", format(wire_volume, ".3f"), "inch3")
+      
+def calculateLength():
+      length = wire_volume/(3.14*wire_radius**2)
+      print("Wire length: ", format(length, ".3f"), "inches")
+
+def calculateWireTurnLength():
+      wire_turn_length = (core_diameter+wire_diameter)*3.14
+      print("wire circumference: ", format(wire_turn_length, ".5f"))
+      
+def calculateTotalTime():
+      total_time = (length/(RPM*wire_turn_length))/60
+      format_weight = format(weight/454, ".2f")
+      format_hours = format(total_time, ".2f")
+      print("Continuous Winder: ", format_weight, "pounds of", wire_diameter, "inch diameter of ", density, "density wire at", RPM,"RPM will last for", format_hours, "hours")
+
+def calculateNumberofLoops():
+      number_of_loops = (string_length/wire_diameter)
+      print("Number of Loops: ", format(number_of_loops, ".2f"), "inches")
+      
+def LengthOfWireOneString():
+      length_of_wire_per_one_string = number_of_loops * wire_turn_length
+      print("Length of wire per one string: ", format(length_of_wire_per_one_string, ".2f"), "inches")
+      
+def stringPerSpool():
+      number_of_string_per_spool = length/length_of_wire_per_one_string
+      print("Strings per Spool:", format(number_of_string_per_spool, ".1f"))
 
 
 
-wire_radius = (1/2) * wire_diameter
+def main():
+      greeting()
+      printDensityList()
 
-print("Radius of the wire: ", wire_radius)
-
-wire_volume = weight/(density*2.54**3)
-
-print("Volume of the wire: ", format(wire_volume, ".3f"), "inch3")
-
-length = wire_volume/(3.14*wire_radius**2)
-
-print("Wire length: ", format(length, ".3f"), "inches")
+main()
 
 
 
-core_diameter = float(input("core diameter in inches: "))
-
-wire_turn_length = (core_diameter+wire_diameter)*3.14
-
-print("wire circumference: ", format(wire_turn_length, ".5f"))
-
-total_time = (length/(RPM*wire_turn_length))/60
-
-format_weight = format(weight/454, ".2f")
-
-format_hours = format(total_time, ".2f")
-
-print("Continuous Winder: ", format_weight, "pounds of", wire_diameter, "inch diameter of ", density, "density wire at", RPM,"RPM will last for",
-
-      format_hours, "hours")
-
-
-
-string_length = float(input("What is the wrap length of the string in inches: "))
-
-number_of_loops = (string_length/wire_diameter)
-
-print("Number of Loops: ", format(number_of_loops, ".2f"), "inches")
-
-length_of_wire_per_one_string = number_of_loops * wire_turn_length
-
-print("Length of wire per one string: ", format(length_of_wire_per_one_string, ".2f"), "inches")
-
-number_of_string_per_spool = length/length_of_wire_per_one_string
-
-print("Strings per Spool:", format(number_of_string_per_spool, ".1f"))
 
 
 
 #Flat wire
 
-wire_thickness = float(input("What is the thickness of the flat wire in inches: "))
+# wire_thickness = float(input("What is the thickness of the flat wire in inches: "))
 
-wire_width = float(input("What is the width of the flat wire in inches: "))
+# wire_width = float(input("What is the width of the flat wire in inches: "))
 
-flat_wire_length = weight/(wire_thickness*wire_width*density**3)
+# flat_wire_length = weight/(wire_thickness*wire_width*density**3)
 
-print("Length of the flat wire: ", format(flat_wire_length, ".2f"), "inches")
-
-
+# print("Length of the flat wire: ", format(flat_wire_length, ".2f"), "inches")
 
 
 
@@ -97,5 +115,7 @@ print("Length of the flat wire: ", format(flat_wire_length, ".2f"), "inches")
 
 
 
-print(input("pause: "))
+
+
+# print(input("pause: "))
 
